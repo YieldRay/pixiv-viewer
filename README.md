cc807936314e679562bd4add465e336ea6c1fd67
YieldRay <yieldray@outlook.com>
2022-09-14 21:54:51 +0800

    prepare to archive

--- README.md
+++ README.md
@@ -1,24 +1,32 @@
 # pixivviewer
 
+练手项目，由于架构稀烂，不再维护。  
+编写时 Vue CLI 还未进入维护模式。
+
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Lints and fixes files
+
 ```
 yarn lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/nav/UserSpace.vue
+++ src/components/nav/UserSpace.vue
@@ -1,37 +1,35 @@
 <template>
-  <div>
-    <slot>
-      <small @click="isOpen = true" style="cursor: pointer"> ⛏关于 </small>
-    </slot>
-  </div>
+    <div>
+        <slot>
+            <small @click="isOpen = true" style="cursor: pointer"> ⛏关于 </small>
+        </slot>
+    </div>
 
-  <teleport to="body">
-    <transition name="fade">
-      <div class="popup" v-if="isOpen">
-        <div class="flex">
-          <strong>关于</strong>
-          <span class="close" @click="isOpen = false"> [关闭] </span>
-        </div>
-        <p>
-          Made by
-          <a href="https://github.com/YieldRay" target="_blank">YieldRay</a>
-        </p>
-        <p>Powered by Vue3</p>
-        <p>
-          实现了Pixiv的简单操作，API来自
-          <a href="https://pixiv.js.org/" target="_blank">pixiv.js.org</a>
-          项目
-        </p>
-      </div>
-    </transition>
-  </teleport>
+    <teleport to="body">
+        <transition name="fade">
+            <div class="popup" v-if="isOpen">
+                <div class="flex">
+                    <strong>关于</strong>
+                    <span class="close" @click="isOpen = false"> [关闭] </span>
+                </div>
+                <p>
+                    <a href="https://github.com/YieldRay/pixiv-viewer">source code</a>
+                </p>
+                <p>
+                    实现了Pixiv的简单操作，API来自
+                    <a href="https://pixiv.js.org/" target="_blank">pixiv.js.org</a>
+                    项目
+                </p>
+            </div>
+        </transition>
+    </teleport>
 </template>
 
 <script>
 export default {
-  data() {
-    return { isOpen: false };
-  },
+    data() {
+        return { isOpen: false };
+    },
 };
 </script>
 
@@ -39,43 +37,43 @@ export default {
 @import url("../../assets/transiton.css");
 
 div {
-  min-width: 40vw;
-  max-width: 100%;
+    min-width: 40vw;
+    max-width: 100%;
 }
 @media screen and (max-width: 32rem) {
-  div {
-    min-width: 10vw;
-  }
+    div {
+        min-width: 10vw;
+    }
 }
 
 .popup {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  border-radius: 8px;
-  padding: 1rem;
-  width: 50vw;
-  min-height: 50vh;
-  background: #fff;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+    padding: 1rem;
+    width: 50vw;
+    min-height: 50vh;
+    background: #fff;
 }
 .popup a {
-  color: cadetblue;
-  transition: all 0.3s;
+    color: cadetblue;
+    transition: all 0.3s;
 }
 
 .popup a:hover {
-  text-decoration: underline;
+    text-decoration: underline;
 }
 
 .close {
-  cursor: pointer;
+    cursor: pointer;
 }
 
 .flex {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 }
 </style>
