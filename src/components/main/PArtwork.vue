62b6baa40441250647f51e298dccc3ab87b4784c
YieldRay <yieldray@outlook.com>
2022-10-09 21:15:58 +0800

    .

--- src/components/main/PArtwork.vue
+++ src/components/main/PArtwork.vue
@@ -2,293 +2,279 @@
 import { proxy, api } from "../../assets/config.js";
 import PImgList from "./PImgList.vue";
 export default {
-  components: {
-    PImgList,
-  },
-  created() {
-    this.$watch(
-      () => this.$route.params,
-      (toParams, previousParams) => {
-        // watch changes of path
-        console.log(toParams, previousParams);
-        this.data = null; // flush
-        this.fetchData(toParams.id);
-        this.recommendList = []; // flush
+    components: {
+        PImgList,
+    },
+    created() {
+        this.$watch(
+            () => this.$route.params,
+            (toParams, previousParams) => {
+                // watch changes of path
+                console.log(toParams, previousParams);
+                this.data = null; // flush
+                this.fetchData(toParams.id);
+                this.recommendList = []; // flush
+                this.fetchRecommend();
+            }
+        );
+    },
+    mounted() {
+        this.fetchData(this.$route.params.id);
         this.fetchRecommend();
-      }
-    );
-  },
-  mounted() {
-    this.fetchData(this.$route.params.id);
-    this.fetchRecommend();
-  },
-  data() {
-    return {
-      data: null,
-      isMobile: Boolean(document.body.offsetWidth < 480),
-      recommendList: [], // ! need init
-      recommendData: [], // ! is an array
-      // only init value
-      pagesData: [], // for pages
-      quality: window.localStorage.getItem("quality") || "regular",
-    };
-  },
-  methods: {
-    async fetchData(id) {
-      this.data = null; // re-flush
-      this.data = await fetch(api(`/ajax/illust/${id}`)).then((res) =>
-        res.json()
-      );
-      this.data.alt && (document.title = this.data.alt);
     },
-    async fetchRecommend() {
-      // init
-      const count = 9; // 一次获取的推荐图片数量
-      if (!this.recommendList || this.recommendList.length === 0) {
-        const { illusts, nextIds } = await fetch(
-          api(`/ajax/illust/${this.$route.params.id}/recommend/init?limit=18`)
-        ).then((res) => res.json());
-        this.recommendData = illusts;
-        this.recommendList = nextIds;
-        return;
-      }
-      // get more
-      const buildURL = (ids) => {
-        let apiAppend = api("/ajax/illust/recommend/illusts?");
-        ids.forEach((id) => (apiAppend += `illust_ids=${id}&`));
-        return apiAppend;
-      };
-      const ids = this.recommendList.splice(0, count); // pop from top
-      const { illusts } = await fetch(buildURL(ids)).then((res) => res.json());
-      this.recommendData.push(...illusts);
+    data() {
+        return {
+            data: null,
+            isMobile: Boolean(document.body.offsetWidth < 480),
+            recommendList: [], // ! need init
+            recommendData: [], // ! is an array
+            // only init value
+            pagesData: [], // for pages
+            quality: window.localStorage.getItem("quality") || "regular",
+        };
     },
-    async fetchMore() {
-      this.pagesData = await fetch(
-        api(`/ajax/illust/${this.$route.params.id}/pages`)
-      ).then((res) => res.json());
+    methods: {
+        async fetchData(id) {
+            this.data = null; // re-flush
+            this.data = await fetch(api(`/ajax/illust/${id}`)).then((res) => res.json());
+            this.data.alt && (document.title = this.data.alt);
+        },
+        open(url) {
+            window.open(url);
+        },
+        async fetchRecommend() {
+            // init
+            const count = 9; // 一次获取的推荐图片数量
+            if (!this.recommendList || this.recommendList.length === 0) {
+                const { illusts, nextIds } = await fetch(
+                    api(`/ajax/illust/${this.$route.params.id}/recommend/init?limit=18`)
+                ).then((res) => res.json());
+                this.recommendData = illusts;
+                this.recommendList = nextIds;
+                return;
+            }
+            // get more
+            const buildURL = (ids) => {
+                let apiAppend = api("/ajax/illust/recommend/illusts?");
+                ids.forEach((id) => (apiAppend += `illust_ids=${id}&`));
+                return apiAppend;
+            };
+            const ids = this.recommendList.splice(0, count); // pop from top
+            const { illusts } = await fetch(buildURL(ids)).then((res) => res.json());
+            this.recommendData.push(...illusts);
+        },
+        async fetchMore() {
+            this.pagesData = await fetch(api(`/ajax/illust/${this.$route.params.id}/pages`)).then((res) => res.json());
+        },
+        proxy,
     },
-    proxy,
-  },
 };
 </script>
 
 <template>
-  <div v-if="!data" class="loading" style="width: 100%; height: 100vh"></div>
-  <main v-if="data">
-    <div class="flex">
-      <div class="container">
-        <figure>
-          <img
-            :src="proxy(data?.urls?.[quality])"
-            nohover
-            class="loading"
-            :class="data.width > data.height ? 'fit-x' : 'fit-y'"
-            v-if="pagesData.length === 0"
-          />
-          <div class="center">
-            <button
-              @click="fetchMore"
-              v-if="data.pageCount > 1 && pagesData.length === 0"
-              class="btn"
-            >
-              加载剩余{{ data.pageCount - 1 }}张图片
-            </button>
-          </div>
+    <div v-if="!data" class="loading" style="width: 100%; height: 100vh"></div>
+    <main v-if="data">
+        <div class="flex">
+            <div class="container">
+                <figure>
+                    <img
+                        :src="proxy(data?.urls?.[quality])"
+                        nohover
+                        class="loading"
+                        :class="data.width > data.height ? 'fit-x' : 'fit-y'"
+                        v-if="pagesData.length === 0"
+                    />
+                    <div class="center">
+                        <button @click="fetchMore" v-if="data.pageCount > 1 && pagesData.length === 0" class="btn">
+                            加载剩余{{ data.pageCount - 1 }}张图片
+                        </button>
 
-          <!-- 分页 -->
-          <transition-group name="list">
-            <img
-              v-for="(page, i) in pagesData"
-              :key="i"
-              :src="proxy(page?.urls?.[quality])"
-              nohover
-              class="loading"
-              :class="data.width > data.height ? 'fit-x' : 'fit-y'"
-            />
-          </transition-group>
-        </figure>
-        <section>
-          <figcaption>
-            <h1>
-              {{ data?.title }}
-            </h1>
-            <ul v-if="data">
-              <li v-for="one in data.tags.tags" :key="one.tag">
-                <template v-if="one">
-                  <router-link :to="`/search/${one.tag}`">
-                    <span> #{{ one.tag }} </span>
-                    <small>{{ one?.translation?.en }}</small>
-                  </router-link>
-                </template>
-              </li>
-            </ul>
-          </figcaption>
-        </section>
-      </div>
-      <!-- 两个container，桌面时flex布局 -->
-      <div class="container">
-        <!-- <img :src="proxy(data.profileImageUrl)" :alt="data.userAccount" /> -->
-        <h2>
-          {{ data.userName }}
-        </h2>
-        <router-link :to="`/users/${data.userId}`">
-          <button class="btn">查看画师详情</button>
-        </router-link>
-        <p style="margin: 1rem; font-size: 0.8rem; font-weight: bolder">
-          其他作品
-        </p>
-        <PImgList
-          :object="data.userIllusts"
-          :height="5"
-          :small="true"
-          :rate="0.5"
-        ></PImgList>
-      </div>
-    </div>
-    <template v-if="recommendData">
-      <PImgList
-        :array="recommendData"
-        :height="isMobile ? 8 : 12"
-        :noscroll="true"
-        :small="false"
-        :gap="1"
-        title="相关作品"
-      ></PImgList>
-      <div class="center">
-        <button v-if="recommendData" class="btn" @click="fetchRecommend">
-          加载更多
-        </button>
-      </div>
-    </template>
-  </main>
+                        <button class="btn" @click="open(proxy(data?.urls?.original))">下载原图(第一张)</button>
+                    </div>
+
+                    <!-- 分页 -->
+                    <transition-group name="list">
+                        <img
+                            v-for="(page, i) in pagesData"
+                            :key="i"
+                            :src="proxy(page?.urls?.[quality])"
+                            nohover
+                            class="loading"
+                            :class="data.width > data.height ? 'fit-x' : 'fit-y'"
+                        />
+                    </transition-group>
+                </figure>
+                <section>
+                    <figcaption>
+                        <h1>
+                            {{ data?.title }}
+                        </h1>
+                        <ul v-if="data">
+                            <li v-for="one in data.tags.tags" :key="one.tag">
+                                <template v-if="one">
+                                    <router-link :to="`/search/${one.tag}`">
+                                        <span> #{{ one.tag }} </span>
+                                        <small>{{ one?.translation?.en }}</small>
+                                    </router-link>
+                                </template>
+                            </li>
+                        </ul>
+                    </figcaption>
+                </section>
+            </div>
+            <!-- 两个container，桌面时flex布局 -->
+            <div class="container">
+                <!-- <img :src="proxy(data.profileImageUrl)" :alt="data.userAccount" /> -->
+                <h2>
+                    {{ data.userName }}
+                </h2>
+                <router-link :to="`/users/${data.userId}`">
+                    <button class="btn">查看画师详情</button>
+                </router-link>
+                <p style="margin: 1rem; font-size: 0.8rem; font-weight: bolder">其他作品</p>
+                <PImgList :object="data.userIllusts" :height="5" :small="true" :rate="0.5"></PImgList>
+            </div>
+        </div>
+        <template v-if="recommendData">
+            <PImgList
+                :array="recommendData"
+                :height="isMobile ? 8 : 12"
+                :noscroll="true"
+                :small="false"
+                :gap="1"
+                title="相关作品"
+            ></PImgList>
+            <div class="center">
+                <button v-if="recommendData" class="btn" @click="fetchRecommend">加载更多</button>
+            </div>
+        </template>
+    </main>
 </template>
-
-
 
 <style scoped>
 main {
-  background: #f5f5f5;
-  min-height: 100%;
-  margin: 0;
-  padding: 0;
+    background: #f5f5f5;
+    min-height: 100%;
+    margin: 0;
+    padding: 0;
 }
 
 @import url("../../assets/loading.css");
 @import url("../../assets/transiton.css");
 
 .container {
-  margin: 2vh 4vw;
-  /* this!!! */
-  box-shadow: 0 1px 1rem 2px rgba(0, 0, 0, 0.01);
-  border-radius: 0.5rem;
-  overflow: hidden;
+    margin: 2vh 4vw;
+    /* this!!! */
+    box-shadow: 0 1px 1rem 2px rgba(0, 0, 0, 0.01);
+    border-radius: 0.5rem;
+    overflow: hidden;
 }
 section {
-  padding: 1rem;
+    padding: 1rem;
 }
 @media screen and (min-width: 48rem) {
-  /* for PC */
-  .flex {
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-  }
-  .container {
-    margin: 1rem;
-  }
-  .container:first-child {
-    margin-left: 3rem;
-  }
-  .container:last-child {
-    margin-right: 3rem;
-    max-width: 20rem;
-  }
-  section {
-    padding: 4rem 8rem;
-  }
+    /* for PC */
+    .flex {
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+    }
+    .container {
+        margin: 1rem;
+    }
+    .container:first-child {
+        margin-left: 3rem;
+    }
+    .container:last-child {
+        margin-right: 3rem;
+        max-width: 20rem;
+    }
+    section {
+        padding: 4rem 8rem;
+    }
 }
 
 figure {
-  background: #fafafa;
-  margin: 0 auto;
-  overflow: hidden;
-  min-height: 30vh;
-  transition: all 0.4s;
+    background: #fafafa;
+    margin: 0 auto;
+    overflow: hidden;
+    min-height: 30vh;
+    transition: all 0.4s;
 }
 
 figure > img {
-  max-width: 100%;
-  max-height: 100vh;
-  margin: auto;
-  display: block;
+    max-width: 100%;
+    max-height: 100vh;
+    margin: auto;
+    display: block;
 }
 img.fit-x {
-  /* 宽图 */
-  width: 100%;
-  height: auto;
+    /* 宽图 */
+    width: 100%;
+    height: auto;
 }
 
 img.fit-y {
-  /* 高图 */
-  height: 100%;
-  width: auto;
+    /* 高图 */
+    height: 100%;
+    width: auto;
 }
 
 section {
-  background: #fff;
+    background: #fff;
 }
 
 h1,
 h2 {
-  margin: 0px 0px 8px;
-  color: rgb(31, 31, 31);
-  font-size: 20px;
-  line-height: 24px;
-  font-weight: bold;
+    margin: 0px 0px 8px;
+    color: rgb(31, 31, 31);
+    font-size: 20px;
+    line-height: 24px;
+    font-weight: bold;
 }
 
 h2 {
-  font-size: 18px;
+    font-size: 18px;
 }
 
 ul {
-  margin: 0px -6px 0px 0px;
-  list-style: none;
-  padding-left: 0px;
-  word-break: break-all;
+    margin: 0px -6px 0px 0px;
+    list-style: none;
+    padding-left: 0px;
+    word-break: break-all;
 }
 
 li {
-  display: inline;
-  margin: 0px 12px 0px 0px;
+    display: inline;
+    margin: 0px 12px 0px 0px;
 }
 li span {
-  color: rgb(61, 118, 153);
+    color: rgb(61, 118, 153);
 }
 li small {
-  display: inline-block;
-  color: rgb(173, 173, 173);
-  font-size: 12px;
-  margin: 0px 0px 0px 4px;
-  padding: 0px;
-  word-break: break-word;
+    display: inline-block;
+    color: rgb(173, 173, 173);
+    font-size: 12px;
+    margin: 0px 0px 0px 4px;
+    padding: 0px;
+    word-break: break-word;
 }
 .btn {
-  width: 16rem;
-  border: none;
-  padding: 0.5rem;
-  border-radius: 1rem;
-  color: inherit;
-  background: #fff;
-  position: relative;
-  cursor: pointer;
+    width: 16rem;
+    border: none;
+    padding: 0.5rem;
+    border-radius: 1rem;
+    color: inherit;
+    background: #fff;
+    position: relative;
+    cursor: pointer;
 }
 .center {
-  display: flex;
-  justify-content: center;
+    display: flex;
+    justify-content: center;
 }
 
 main {
-  padding-bottom: 1rem;
+    padding-bottom: 1rem;
 }
 </style>
--- src/components/nav/PSidebar.vue
+++ src/components/nav/PSidebar.vue
@@ -1,111 +1,111 @@
 <template>
-  <transition name="fade">
-    <div class="mask" v-if="on" @click="$emit('toggle')"></div>
-  </transition>
+    <transition name="fade">
+        <div class="mask" v-if="on" @click="$emit('toggle')"></div>
+    </transition>
 
-  <div class="sidebar" :class="{ ltr: on, rtl: !on }">
-    <header>
-      <RoundButton :on="on" @click="$emit('toggle')"></RoundButton>
-      <PLogo></PLogo>
-    </header>
-    <h2>
-      画质调节<small>(仅对图片详情生效)</small>
-      <br />
-      <small> 当前: {{ quality }} </small>
-    </h2>
-    <ul>
-      <li @click="setQuality('thumb_mini')">thumb_mini</li>
-      <li @click="setQuality('small')">small</li>
-      <li @click="setQuality('regular')">regular</li>
-      <li @click="setQuality('original')">original</li>
-    </ul>
-  </div>
+    <div class="sidebar" :class="{ ltr: on, rtl: !on }">
+        <header>
+            <RoundButton :on="on" @click="$emit('toggle')"></RoundButton>
+            <PLogo></PLogo>
+        </header>
+        <h2>
+            画质调节<small>(仅对图片详情生效)</small>
+            <br />
+            <small> 当前: {{ quality }} </small>
+        </h2>
+        <ul>
+            <li @click="setQuality('thumb_mini')">thumb_mini</li>
+            <li @click="setQuality('small')">small</li>
+            <li @click="setQuality('regular')">regular</li>
+            <li @click="setQuality('original')">original</li>
+        </ul>
+    </div>
 </template>
 
 <script>
 import RoundButton from "./ToggleButton.vue";
 import PLogo from "./PLogo.vue";
 export default {
-  name: "PSidebar",
-  props: {
-    on: Boolean,
-  },
-  emits: ["toggle"],
-  components: {
-    RoundButton,
-    PLogo,
-  },
-  data() {
-    return {
-      quality: window.localStorage.getItem("quality") || "regular",
-    };
-  },
-  methods: {
-    setQuality(q) {
-      window.localStorage.setItem("quality", q);
-      this.quality = q;
-      console.log("quality set to " + window.localStorage.getItem("quality"));
+    name: "PSidebar",
+    props: {
+        on: Boolean,
     },
-  },
+    emits: ["toggle"],
+    components: {
+        RoundButton,
+        PLogo,
+    },
+    data() {
+        return {
+            quality: window.localStorage.getItem("quality") || "regular",
+        };
+    },
+    methods: {
+        setQuality(q) {
+            window.localStorage.setItem("quality", q);
+            this.quality = q;
+            location.reload();
+        },
+    },
 };
 </script>
 
 <style scoped>
 .sidebar {
-  position: absolute;
-  left: 0;
-  top: 0;
-  height: 100vh;
-  width: 15rem;
-  box-shadow: 1px 0px 2px rgba(0, 0, 0, 0.2);
-  background: #fff;
-  transition: all 0.3s;
-  z-index: 3;
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100vh;
+    width: 15rem;
+    box-shadow: 1px 0px 2px rgba(0, 0, 0, 0.2);
+    background: #fff;
+    transition: all 0.3s;
+    z-index: 3;
 }
 .ltr {
-  transform: translateX(0);
+    transform: translateX(0);
 }
 .rtl {
-  transform: translateX(-100%);
+    transform: translateX(-100%);
 }
 header {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  padding-left: 1vw;
-  height: 3.5rem;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    padding-left: 1vw;
+    height: 3.5rem;
 }
 
 .mask {
-  position: absolute;
-  left: 0;
-  top: 0;
-  height: 100vh;
-  width: 100vw;
-  background: rgba(0, 0, 0, 0.1);
-  z-index: 2;
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100vh;
+    width: 100vw;
+    background: rgba(0, 0, 0, 0.1);
+    z-index: 2;
 }
 
 ul {
-  display: flex;
-  flex-flow: column;
-  margin: 0px;
-  padding: 0px;
-  list-style: none;
+    display: flex;
+    flex-flow: column;
+    margin: 0px;
+    padding: 0px;
+    list-style: none;
 }
 li {
-  padding: 12px 0 12px 24px;
-  transition: all 0.2s ease 0s;
+    padding: 12px 0 12px 24px;
+    transition: all 0.2s ease 0s;
 }
 li:hover {
-  background: rgba(0, 0, 0, 0.04);
+    background: rgba(0, 0, 0, 0.04);
 }
 h2 {
-  margin-left: 24px;
-  font-size: 14px;
-  line-height: 22px;
-  font-weight: bold;
-  color: rgb(133, 133, 133);
+    margin-left: 24px;
+    font-size: 14px;
+    line-height: 22px;
+    font-weight: bold;
+    color: rgb(133, 133, 133);
 }
 @import url("../../assets/transiton.css");
 </style>
